5223e92305d5bb6de4f07d499b28288873aafe9a
widya198 <widiangrm@gmail.com>
2017-10-17 09:59:43 +0700

    Update Tugas32.java

--- src/tugas32/Tugas32.java
+++ src/tugas32/Tugas32.java
@@ -1,21 +1,90 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tugas32;
 
 /**
  *
  * @author Widya
  */
-public class Tugas32 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class Tugas32
+{
+    final int N = 4;
+ 
+  
+    void printSolution(int sol[][])
+    {
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+                System.out.print(" " + sol[i][j] +
+                                 " ");
+            System.out.println();
+        }
     }
-    
+ 
+    boolean isSafe(int maze[][], int x, int y)
+    {
+        
+        return (x >= 0 && x < N && y >= 0 &&
+                y < N && maze[x][y] == 1);
+    }
+ 
+    boolean solveMaze(int maze[][])
+    {
+        int sol[][] = {{0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+        };
+ 
+        if (solveMazeUtil(maze, 0, 0, sol) == false)
+        {
+            System.out.print("Solution doesn't exist");
+            return false;
+        }
+ 
+        printSolution(sol);
+        return true;
+    }
+ 
+    boolean solveMazeUtil(int maze[][], int x, int y,
+                          int sol[][])
+    {
+      
+        if (x == N - 1 && y == N - 1)
+        {
+            sol[x][y] = 1;
+            return true;
+        }
+ 
+        if (isSafe(maze, x, y) == true)
+        {
+           
+            sol[x][y] = 1;
+ 
+           
+            if (solveMazeUtil(maze, x + 1, y, sol))
+                return true;
+ 
+           
+            if (solveMazeUtil(maze, x, y + 1, sol))
+                return true;
+ 
+           
+            sol[x][y] = 0;
+            return false;
+        }
+ 
+        return false;
+    }
+ 
+    public static void main(String args[])
+    {
+        Tugas32 rat = new Tugas32();
+        int maze[][] = {{1, 0, 0, 0},
+            {1, 1, 0, 1},
+            {0, 1, 0, 0},
+            {1, 1, 1, 1}
+        };
+        rat.solveMaze(maze);
+    }
 }
